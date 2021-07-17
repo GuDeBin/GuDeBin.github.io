1dd526f3958f5f551058b8c82bef20d606edc929
GuDeBin <gudebin@outlook.com>
2021-07-17 15:37:10 +0800

    适配

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const projects = [
     number: 1,
     name: "expanding-cards",
     imgLink: "1-expanding-cards.png",
+    url: "expanding-cards",
   },
 ];
 
@@ -12,11 +13,19 @@ projects.forEach((project) => {
   projectEl = document.createElement("div");
   projectEl.innerHTML = `
   <span class="day">Dya ${project.number}</span>
-  <img src="public/images/${project.imgLink}" alt="" />
-  <div class="content">
-    <h4>${formateProjectName(project.name)}</h4>
-    <a href="projects/${project.name}/index.html" target="_blank">Live Demo</a>
-  </div>
+        <img src="public/images/${project.imgLink}" alt="" />
+        <div class="content">
+          <h4>${formateProjectName(project.name)}</h4>
+          <a href="projects/${project.url}/index.html" target="_blank"
+            >Live Demo</a
+          >
+        </div>
+        <a
+          class="iphoneBtn"
+          href="projects/${project.url}/index.html"
+          target="_blank"
+          >Live Demo</a
+        >
   `;
   projectsEl.appendChild(projectEl);
 });
